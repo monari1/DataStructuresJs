0ecafdc68b4723a04f919f8ff3621cf8034ac2f7
monari1 <francisnyasinga@gmail.com>
2023-05-13 12:34:51 +0300

    working on order of logical operators from if, to else if to else

--- freecode.js
+++ freecode.js
@@ -17,16 +17,29 @@
 
 // testing if, else if and else
 
-function testElseIf(val) {
-    if (val > 10) {
-        return "Greater than 10";
-    } else if (val < 5) {
-        return "Smaller than 5";
+// function testElseIf(val) {
+//     if (val > 10) {
+//         return "Greater than 10";
+//     } else if (val < 5) {
+//         return "Smaller than 5";
+//     } else {
+//         return "Between 5 and 10";
+//     }
+
+
+// }
+
+// console.log(testElseIf(50));
+
+// the logical flow of logical operations of if, else if and else
+function orderMyLogic(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10";
     } else {
-        return "Between 5 and 10";
+        return "Greater than or equal to 10";
     }
-
-
 }
 
-console.log(testElseIf(50));
+console.log(orderMyLogic(30));
